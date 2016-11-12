91ad4daf083eb080bcef30664320f3bdc5cb935d
Dan Finelli <dan.finelli@hotmail.com>
2016-11-11 17:27:10 -0800

    api logic 80%

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,72 @@
 console.log("Sanity Check: JS is working!");
+$(document).ready(function() {
 
-$(document).ready(function(){
+    var template;
+    var $strainList = $('#strainTarget');
+    var allStrains = [];
 
-// your code
+    var $strainsList = $('#strainTarget');
 
+    //compile handlebars
+    var source = $('#strains-template').html();
+    template = Handlebars.compile(source);
+
+    $.ajax({
+        method: 'GET',
+        url: '/api/strains',
+        success: handleSuccess
+    });
+
+    $strainsList.on('click', '.deleteBtn', function() {
+        console.log('clicked delete button to', '/api/strains/' + $(this).attr('data-id'));
+        $.ajax({
+            method: 'DELETE',
+            url: '/api/strains/' + $(this).attr('data-id'),
+            success: deleteStrainSuccess
+        });
+    });
+
+    $('#newStrainForm').on('submit', function(event) {
+        event.preventDefault();
+        var showData = $('#show-data');
+        console.log('it got here');
+
+        $.ajax({
+            url: '/api/strains',
+            method: 'POST',
+            data: $('#newStrainForm').serialize(),
+            dataType: 'json',
+            handleSuccess
+        });
+    });
+
+    function render() {
+        $strainsList.empty();
+        var strainsHtml = template({
+            strains: allStrains
+        });
+        $strainsList.append(strainsHtml);
+    }
+
+    function handleSuccess(data) {
+        //Do stuff with the JSON data
+        console.log(data);
+        allStrains = data;
+        render();
+    }
+
+    function deleteStrainSuccess(json) {
+      var strain = json;
+      console.log(strain);
+      var strainId = stain._id;
+      console.log('delete book', strainId);
+      // find the book with the correct ID and remove it from our allBooks array
+      for(var index = 0; index < allStrains.length; index++) {
+        if(allStrains[index]._id === strainId) {
+          allStrains.splice(index, 1);
+          break;  // we found our book - no reason to keep searching (this is why we didn't use forEach)
+        }
+      }
+      render();
+    }
 });
--- server.js
+++ server.js
@@ -5,21 +5,23 @@ var express = require('express'),
 // parse incoming urlencoded form data
 // and populate the req.body object
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // allow cross origin requests (optional)
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 /************
  * DATABASE *
  ************/
 
- var db = require('./models');
+var db = require('./models');
 
 /**********
  * ROUTES *
@@ -32,13 +34,9 @@ app.use(express.static('public'));
 /*
  * HTML Endpoints
  */
- app.get('/api/strains', function (req, res) {
-   // send all books as JSON response
-   db.Strains.find().populate('strains')
-     .exec(function(err, strains) {
-       if (err) { return console.log("index error: " + err); }
-       res.json(strains);
-   });
+
+ app.get('/', function homepage(req, res) {
+   res.sendFile(__dirname + '/views/index.html');
  });
 
 
@@ -48,37 +46,76 @@ app.use(express.static('public'));
  */
 
 app.get('/api', function api_index(req, res) {
-  // TODO: Document all your api endpoints below
+    // TODO: Document all your api endpoints below
+    res.json({
+        woopsIForgotToDocumentAllMyEndpoints: false, // done
+        message: "Welcome to my personal api! Here's what you need to know!",
+        documentationUrl: "https://github.com/danfinelli1/express-personal-api/blob/master/README.md", // TODO
+        baseUrl: "https://boiling-bastion-91533.herokuapp.com",
+        endpoints: [{
+                method: "GET",
+                path: "/api",
+                description: "Describes all available endpoints"
+            }, {
+                method: "GET",
+                path: "/api/profile",
+                description: "Data about me"
+            }, // CHANGE ME
+            {
+                method: "GET",
+                path: "/api/strains",
+                description: "View strains api"
+            } // CHANGE ME
+        ]
+    });
+});
+
+app.get('/api/profile', function profile(req, res){
   res.json({
-    woopsIForgotToDocumentAllMyEndpoints: false, // done
-    message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/danfinelli1/express-personal-api/blob/master/README.md", // TODO
-    baseUrl: "https://boiling-bastion-91533.herokuapp.com",
-    endpoints: [
-      {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "GET", path: "/api/strains", description: "View strains api"} // CHANGE ME
-    ]
-  })
+          name: "Daniel Finelli",
+          githubLink: "http://github.com/danfinelli1",
+          githubProfileImage: '',
+          personalSiteLink: '',//TODO
+          currentCity: "San Francisco",
+          isAwake: true,
+          familyMembers: [
+            { name: 'Maria Finelli', relationship: 'sister' }
+          ],
+          pets: null
+        });
 });
 
-app.get('/', function homepage(req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-app.post('/api/strains', function (req, res) {
-  // create new book with form data (`req.body`)
-  var newStrain = new db.Strains({
-    title: req.body.title,
-    image: req.body.image,
-    releaseDate: req.body.releaseDate,
+app.get('/api/strains', function(req, res){
+  db.Strains.find()
+    .exec(function(err, strains) {
+      if (err) { return console.log("index error: " + err); }
+      res.json(strains);
   });
+});
 
+app.post('/api/strains', function(req, res) {
+    // create new strain with form data (`req.body`)
+    var newStrain = new db.Strains({
+        name: req.body.name,
+        tastes: req.body.tastes,
+        outdoorGrown: req.body.outdoorGrown,
+        smellScale: req.body.smellScale,
+        treats: req.body.treats
+    });
+    newStrain.save(function(err, strains) {
+        if (err) {
+            return console.log("save error: " + err);
+        }
+        console.log("saved ", strains.name);
+        // send back the strain!
+        res.json(strains);
+    });
+});
 /**********
  * SERVER *
  **********/
 
 // listen on port 3000
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Express server is up and running on http://localhost:3000/');
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Express server is up and running on http://localhost:3000/');
 });
