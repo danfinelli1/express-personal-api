80d97b13094d35c3ae37875585019098b26b31c0
Dan Finelli <dan.finelli@hotmail.com>
2016-11-11 22:33:36 -0800

    messed something in seed up2

--- seed.js
+++ seed.js
@@ -6,45 +6,8 @@ var new_strains = {
         outdoorGrown: true,
         smellScale: 9,
         treats: "severe pain, nausea, and appetite loss"
-    }, {
-        name: "White Widow",
-        tastes: "earthy, woody, and pungent",
-        outdoorGrown: true,
-        smellScale: 9,
-        treats: "stress, depression, pain, fatigue, and insomnia"
-    }, {
+    };
 
-        name: "Northern Lights",
-        tastes: "earthy, sweet, and pine",
-        outdoorGrown: false,
-        smellScale: 7,
-        treats: "stress, pain, insomnia, depression, and headaches"
-
-    }, {
-
-        name: "Strawberry Cough",
-        tastes: "strawberry, sweet, and berry",
-        outdoorGrown: false,
-        smellScale: 8,
-        treats: "stress, depression, pain, fatigue, and lack of appetite"
-
-    }, {
-
-        name: "Blue Cheese",
-        tastes: "blue cheese, pungent, and cheese",
-        outdoorGrown: false,
-        smellScale: 10,
-        treats: "muscle spasms, pain, and stress"
-
-    }, {
-
-        name: "Granddaddy Purple",
-        tastes: "grape, berry, and sweet",
-        outdoorGrown: true,
-        smellScale: 6,
-        treats: "pain, stress, insomnia, appetite loss, and muscle spasms"
-
-};
 
 db.Strains.create(new_strains, function(err, strains) {
     if (err) {
